229ecfd61d8f8deb404429426af17591dd31cebc
Tessu Johny <tessujohny.tj@gmail.com>
2022-01-17 15:37:46 +0300

    A UI with functioning search bar using React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+import React, {Fragment, Component} from 'react';
+import SearchBox from './SearchBox';
+import CardList from './CardList';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            robots : [],
+            searchInput : ''
+        };
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(json => this.setState({robots: json}));
+    }
+
+    onSearchChange = (event) => {
+        this.setState({searchInput: event.target.value});
+    }
+
+    render() {
+        const selectedRobots = this.state.robots.filter((robot) => {
+            return robot.name.toLowerCase().includes(this.state.searchInput.toLowerCase());
+        });
+        if (this.state.robots.length === 0) {
+            return (<h1>L o a d i n g...</h1>);
+        } else {
+            return (
+                <div className='tc'>
+                    <h1 className='f1'>R o b o f r i e n d s</h1>
+                    <SearchBox onSearchChange={this.onSearchChange} />
+                    <CardList robots={selectedRobots} />
+                </div>
+            );
+        }
+    }
 }
 
 export default App;
---
+++ src/CardList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Card from './Card';
+
+const CardList = ({ robots }) => {
+    return (
+        robots.map((robot, i) => {
+            return(
+                <Card key={robots[i].id} name={robots[i].name} email={robots[i].email} id={robots[i].id} />
+            );
+        })
+    );
+}
+
+export default CardList;
